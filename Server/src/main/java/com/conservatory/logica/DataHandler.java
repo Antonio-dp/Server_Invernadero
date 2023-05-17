4ce87f1a237789688e51e84cf6413b831730f7cf
Vastem <vastemxt@gmail.com>
2023-05-17 00:15:03 -0700

    Update DataHandler.java

--- Server/src/main/java/com/conservatory/logica/DataHandler.java
+++ Server/src/main/java/com/conservatory/logica/DataHandler.java
@@ -24,23 +24,21 @@ public class DataHandler {
     public void handleData(Registro r) {
         System.out.println("Entro al data handler");
         for (Alarma alarma : alarmas) {
-            if(alarma.getSensor().equals(r.getSensor().getId())){
-                switch(alarma.getTipo()){
-                    case "HUMEDAD":
-                        System.out.println("entro a humedad");
-                        System.out.println("humedad: " + r.getHumedad());
-                        System.out.println("limites: " + alarma.getLimiteInferior() + "-" + alarma.getLimiteSuperior());
+            switch(alarma.getTipo()){
+                case "HUMEDAD":
+                    System.out.println("entro a humedad");
+                    System.out.println("humedad: " + r.getHumedad());
+                    System.out.println("limites: " + alarma.getLimiteInferior() + "-" + alarma.getLimiteSuperior());
 
-                        if (r.getHumedad() < alarma.getLimiteInferior() || r.getHumedad() > alarma.getLimiteSuperior()) {
-                            notificaciones.activarAlarma(alarma);
-                        }
-                        break;
-                    case "TEMPERATURA":
-                        if (r.getTemperatura() < alarma.getLimiteInferior() || r.getTemperatura() > alarma.getLimiteSuperior()) {
-                            notificaciones.activarAlarma(alarma);
-                        }
-                        break;
-                }
+                    if (r.getHumedad() < alarma.getLimiteInferior() || r.getHumedad() > alarma.getLimiteSuperior()) {
+                        notificaciones.activarAlarma(alarma);
+                    }
+                    break;
+                case "TEMPERATURA":
+                    if (r.getTemperatura() < alarma.getLimiteInferior() || r.getTemperatura() > alarma.getLimiteSuperior()) {
+                        notificaciones.activarAlarma(alarma);
+                    }
+                    break;
             }
         }
         nuevoRegistro(r);
